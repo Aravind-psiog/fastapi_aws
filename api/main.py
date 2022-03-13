8272b8a21ebbaa4c8d6b6fb36b40a84af580b0a7
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-03-13 22:01:41 +0530

    re commit

--- api/main.py
+++ api/main.py
@@ -2,7 +2,8 @@ from mangum import Mangum
 from fastapi import FastAPI
 import os
 
-app = FastAPI()
+#app = FastAPI()
+app = FastAPI(root_path="/dev/") #only used for aws for loading Swagger doc. Doesnot work on windows
 
 #
 @app.get("/")
