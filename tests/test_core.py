6ee0498c335f5d08318749b4f89a67f0f5c793e7
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-11-14 11:24:28 +0530

    updated testcases

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,7 +3,8 @@ from api.main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from aws."}
+    assert response.json() == {"message": "Hello from aws updated."}
